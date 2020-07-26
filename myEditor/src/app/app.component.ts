b4e9c7fa375d7e0a6b196b15358563e49fa4b90b
josueJimenez97 <josuejnumss97@gmail.com>
2020-07-26 17:45:49 -0400

    terminar/reanudar

--- myEditor/src/app/app.component.ts
+++ myEditor/src/app/app.component.ts
@@ -14,13 +14,14 @@ export class AppComponent {
   italicOn=false;
   ordenarOn=false;
   justify=1;
+  habilitado=true;
   colorActual="#000000";
   tam=4;
   public constructor(){
   }
   
   ngOnInit(): void {
-    
+    $('#miDiv').attr('contenteditable','true');
     
   }
   focus(){
@@ -29,60 +30,99 @@ export class AppComponent {
   }
 
   bold(){
-    this.boldOn=!this.boldOn;
-    document.execCommand('bold');
-    this.focus();
+    if(this.habilitado){
+      this.boldOn=!this.boldOn;
+      document.execCommand('bold');
+      this.focus();
+    }
+    
    
   }
 
   italic(){
-    this.italicOn=!this.italicOn;
-    document.execCommand('italic');this.focus();
+    if(this.habilitado){
+      this.italicOn=!this.italicOn;
+      document.execCommand('italic');
+      this.focus();
+    }
+    
   }
   
   seleccionColor(e){
-    console.log(e);
-    this.colorActual=e;
-    document.execCommand('foreColor', false, this.colorActual);
-    this.focus();
+    if(this.habilitado){
+      console.log(e);
+      this.colorActual=e;
+      document.execCommand('foreColor', false, this.colorActual);
+      this.focus();
+    }
   }
 
   hizoClick(e){
-    let val= $("#"+e).text();
-    console.log($("#"+e).text())
-    this.tam=parseInt(val);
-    document.execCommand('fontSize', false, val);
-    this.focus();
+    if(this.habilitado){
+      let val= $("#"+e).text();
+      console.log($("#"+e).text())
+      this.tam=parseInt(val);
+      document.execCommand('fontSize', false, val);
+      this.focus();
+    }
   }
 
   ordenarNumericamente(){
-    this.ordenarOn=!this.ordenarOn;
-    console.log("intentando ordenar xd")
-    document.execCommand('insertOrderedList');
-    this.focus();
+    if(this.habilitado){
+      this.ordenarOn=!this.ordenarOn;
+      console.log("intentando ordenar xd")
+      document.execCommand('insertOrderedList');
+      this.focus();
+    }
   }
 
   alignLeft(){
-    this.justify=this.justify==1?0:1;
-    document.execCommand('justifyLeft');
-    this.focus();
+    if(this.habilitado){
+      this.justify=this.justify==1?0:1;
+      document.execCommand('justifyLeft');
+      this.focus();
+    }
+    
   }
   alignCenter(){
-    this.justify=this.justify==2?0:2;
-    document.execCommand('justifyCenter');
-    this.focus();
+    if(this.habilitado){
+      this.justify=this.justify==2?0:2;
+      document.execCommand('justifyCenter');
+      this.focus();
+    }
+    
   }
   alignFull(){
-    this.justify=this.justify==3?0:3;
-    document.execCommand('justifyFull');
-    this.focus();
+    if(this.habilitado){
+      this.justify=this.justify==3?0:3;
+      document.execCommand('justifyFull');
+      this.focus();
+    }
+    
   }
   alignRight(){
-    this.justify=this.justify==4?0:4;
-    document.execCommand('justifyRight');
-    this.focus();
+    if(this.habilitado){
+      this.justify=this.justify==4?0:4;
+      document.execCommand('justifyRight');
+      this.focus();
+    }
   }
   insertarImagen(){
-    document.execCommand('insertimage',null,'https://folimp.com/images/gato/crecen-los-gatos.jpg');
+    if(this.habilitado){
+      
+      document.execCommand('insertimage',null,'https://folimp.com/images/gato/crecen-los-gatos.jpg');
+    }
+  }
+  terminar(){
+    this.habilitado=false;
+    $('#miDiv').attr('contenteditable','false',);
+    $("#titulo").attr('readonly','true');
+    $(".dropdown-toggle").attr('disabled','true');
+  }
+  reanudar(){
+    this.habilitado=true;
+    $('#miDiv').attr('contenteditable','true');
+    $("#titulo").removeAttr('readonly');
+    $(".dropdown-toggle").removeAttr('disabled');
   }
 }
